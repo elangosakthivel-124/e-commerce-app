0a3163db056e88db7a73fc00f4242ab3137f83b3
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-21 23:37:42 +0530

    Create products.component.ts

---
+++ components/product-card/products.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html'
+})
+export class ProductsComponent implements OnInit {
+
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+  }
+}
