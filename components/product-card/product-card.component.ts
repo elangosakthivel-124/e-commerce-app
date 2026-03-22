08985733bc27f6502c420e4d333015e977fe4bdf
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-22 21:10:26 +0530

    Update product-card.component.ts

--- components/product-card/product-card.component.ts
+++ components/product-card/product-card.component.ts
@@ -1,11 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../models/product';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-product-card',
-  templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.css']
+  templateUrl: './product-card.component.html'
 })
 export class ProductCardComponent {
+
   @Input() product!: Product;
+
+  constructor(private cartService: CartService) {}
+
+  addToCart() {
+    this.cartService.addToCart(this.product);
+    alert('Added to cart!');
+  }
 }
