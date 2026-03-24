a3428b453bbf229c8b2f6ec642affa8303b34fcd
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-24 23:19:51 +0530

    Update products.component.ts

--- pages/products/products.component.ts
+++ pages/products/products.component.ts
@@ -10,4 +10,32 @@ export class ProductsComponent {
     { name: 'Phone', price: 20000 },
     { name: 'Headphones', price: 2000 }
   ];
+}import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html'
+})
+export class ProductsComponent implements OnInit {
+
+  products: Product[] = [];
+  loading = true;
+  error = '';
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Failed to load products';
+        this.loading = false;
+      }
+    });
+  }
 }
