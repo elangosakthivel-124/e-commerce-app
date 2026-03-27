6dfa34c03ade037e41db1cb8d3af3d0d708b3668
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-27 23:40:34 +0530

    Create register.component.ts

---
+++ register.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html'
+})
+export class RegisterComponent {
+
+  user = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  register() {
+    this.auth.register(this.user);
+    this.router.navigate(['/login']);
+  }
+}
