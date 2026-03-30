61c1bf2c7312a92df553906006346665ba870c40
Elango sakthivel <elangosakthivel0@gmail.com>
2026-03-30 21:10:28 +0530

    Update auth.interceptor.ts

--- interceptors/auth.interceptor.ts
+++ interceptors/auth.interceptor.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  intercept(req: any, next: any) {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+
     const token = localStorage.getItem('token');
 
     if (token) {
-      const cloned = req.clone({
-        headers: req.headers.set('Authorization', `Bearer ${token}`)
+      const clonedReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
       });
-      return next.handle(cloned);
+      return next.handle(clonedReq);
     }
 
     return next.handle(req);
